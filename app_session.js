0dd265de6027f6043444e605e0743d30cadea9e6
K-Roon <66551757+K-Roon@users.noreply.github.com>
2021-12-15 18:23:56 +0900

    Update app_session.js

--- app_session.js
+++ app_session.js
@@ -1,6 +1,27 @@
 var express = require('express');
+var session = require('express-session');
 var app = express();
+app.use(session({
+    secret: 'rlawoendrotoRlshangusTlqtoRl',
+
+    //session Id를 접속할 때 마다 새로운 것을 발급 여부
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.get('/count', function(req, res){
+    if(req.session.count) {
+        req.session.count ++;
+    } else {
+        req.session.count = 1;
+    }
+    
+    res.send('result : ' + req.session.count);
+});
+app.get('/tmp', function(req, res){
+    res.send('result : ' + req.session.count);
+});
 app.listen(3005, function() {
-    console.log('Connected 3003 port!!');
+    console.log('Connected 3005 port!!');
 });
 
